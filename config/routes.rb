f692d72118a2ec8ead0ce76ec4d0549ca76b0ab2
Jonathan <jonathanbek93@gmail.com>
2019-08-20 11:50:47 +0800

    filled in controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :salons
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'hair#index'
+  root 'salons#index'
   get '/salons' => 'salons#index', as: 'salons'
 
 
