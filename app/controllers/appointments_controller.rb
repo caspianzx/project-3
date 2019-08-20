1b440cdb134f886ef63303f54a27261b62e9c9e4
Jonathan <jonathanbek93@gmail.com>
2019-08-20 15:02:57 +0800

    did appts new and show (pending services to save)

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,6 @@
 class AppointmentsController < ApplicationController
   def index
+
   end
 
   def show
@@ -12,11 +13,26 @@ class AppointmentsController < ApplicationController
   end
 
   def create
+    puts appointment_params[:name]
+    @appointment = Appointment.new(appointment_params)
+    # @appointment.service = Service.find(appointment_params[:service_id])
+    # if @appointment.save
+    #   puts 'Booked!'
+    #   puts @appointment
+    #   redirect_to 'appointments'
+    # else
+    #   puts 'Booking failed!'
+    #   redirect_to 'new_appointment'
+    # end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def appointment_params
+    params.require(:appointment).permit(:name, :phone, :email, :date, :timeslot, :service_id)
   end
 end
