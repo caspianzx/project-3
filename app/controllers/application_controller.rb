0f251ead4b9b23f360e96670ef1eff8573ded101
aliciawongg <w.alicia@gmail.com>
2019-08-25 03:28:33 +0800

    added seed data for salon, detail, service.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-  #stored_location_for(resource) || welcome_path
-   id = current_salon.id.to_s
-    puts 'checking sign in success'
 
-    # stored_location_for(resource) || '/salons/'+id+'/new'
-
-    #stored_location_for(resource) || '/salons/:id/new'
-    '/salons/'+id
 end
 end
