62f9a745561de1c6709570d7dea97057277db920
Jonathan <jonathanbek93@gmail.com>
2019-08-25 07:31:05 +0800

    moved photos from salons ctrler to its own ctrler

--- app/controllers/salons_controller.rb
+++ app/controllers/salons_controller.rb
@@ -65,39 +65,7 @@ class SalonsController < ApplicationController
   def destroy
   end
 
-  def newphoto
-    @salon = Salon.find(params[:id])
-  end
 
-  def createphoto
-    @salon = Salon.find(params[:id])
-    # puts photo_params[:photo_url]
-    @photo = Photo.new(photo_params)
-    @photo.salon = @salon
-    @photo.save
-    if @photo.save
-      puts 'worked'
-      redirect_to photos_path
-    else
-      puts 'try again'
-      redirect_to 'new_photo'
-    end
-  end
-
-  def showphoto
-    @salon = Salon.find(params[:id])
-    # puts @salon.photos.first.photo_url
-    @photos = @salon.photos
-  end
-
-  def destroyphoto
-    @photo = Photo.find(params[:photo_id])
-    # puts @photo.photo_url
-    @salon = Salon.find(params[:salon_id])
-    # puts @salon.id
-    @photo.destroy
-    redirect_to :controller => 'salons', :action => 'showphoto', :id => @salon.id
-  end
 
   def newrating
     @salon = Salon.find(params[:id])
